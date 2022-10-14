8b23dc376bdef4cacc8d2f7a0fa89da5dc29688e
Elizeu RS <13ders@gmail.com>
2022-10-14 16:57:52 -0300

    Creating User Interface

---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Elizeu RS on 13/10/22.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Elizeu RS on 13/10/22.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      view.backgroundColor = .systemPink
+    }
+
+}
